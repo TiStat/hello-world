506e586a7c0d088269630c40adfeb0a91c420987
Tim Ruhkopf <tim.ruhkopf@outlook.de>
2018-08-08 11:48:32 +0200

    imputation plot function with optional boxes or points. NOT a class method yet!

--- R/temp_plot imputations.R
+++ R/temp_plot imputations.R
@@ -1,24 +1,41 @@
-# library(gamlss.cens)
-# gen.cens()
+# NOT working example yet
+# data = simulateData(method ='right' ,
+#                     n = 1000,
+#                     ymu.formula = ~ 0.5*x1,
+#                     ysigma.formula ~ 0.3*x2,
+#                     family = 'NO')
 #
-# x = with(lung, Surv(time, status))
-# ggplot(data = data.frame(x = with(lung, Surv(time, status))), aes(x =x))+
-#   stat_function(fun = dNOrc, args = list(mu = mean(x), sigma = sd(x)))
-# dNOrc(x, mu = mean(x), sigma = sd(x))
-# mean(x)
+# d2 <- imputex(data = data$defected,
+#               xmu_formula= x1~y+x2,
+#               xsigma_formula = ~1,
+#               xnu_formula = ~1,
+#               xtau_formula = ~1,
+#               xfamily = NO(mu.link = 'identity'),
+#               indicator = "indicator",
+#               censtype )
 
 # (Visualize draws) ------------------------------------------------------------------------------
 require(ggplot2)
 require(reshape2)
 
-df = r$imputex$imputations
-df$observation = seq(1, nrow(df)) # longformat index
-df <- melt(df ,  id.vars = 'observation', variable.name = 'proposalVec')
+# make it a class method!
+plotimputations <- function(df, boxes = TRUE) {
 
-ggplot(df, aes(observation,value)) +
-  geom_boxplot(aes(group=observation)) +
-  ylab('Proposals for observation [i]')
+  # Convert to Longformat
+  df$observation = seq(1, nrow(df))
+  df <- melt(df ,  id.vars = 'observation', variable.name = 'proposalVec')
 
-ggplot(df, aes(observation,value)) +
-  geom_point() +
-  ylab('Proposals for observation [i]')
+  if (boxes) {
+    return(ggplot(df, aes(observation, value)) +
+             geom_boxplot(aes(group = observation)) +
+             ylab('Proposals for observation [i]'))
+  }else {
+    return(ggplot(df, aes(observation, value)) +
+             geom_point() +
+             ylab('Proposals for observation [i]'))
+  }
+}
+
+plotimputations(r$imputex$imputations[, -ncol(r$imputex$imputations)],
+                boxes = FALSE)  # the imputed value is ommited
+
