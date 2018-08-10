ffaa0cbee91efaee2862c4f567f94551584fc3c9
Tim Ruhkopf <tim.ruhkopf@outlook.de>
2018-08-10 11:35:26 +0200

    added mean to points, changed input: the imputationsmatrix of imputedx containing the imputed average vector, which is now displayed in red

--- R/temp_plot imputations.R
+++ R/temp_plot imputations.R
@@ -1,19 +1,3 @@
-# NOT working example yet
-# data = simulateData(method ='right' ,
-#                     n = 1000,
-#                     ymu.formula = ~ 0.5*x1,
-#                     ysigma.formula ~ 0.3*x2,
-#                     family = 'NO')
-#
-# d2 <- imputex(data = data$defected,
-#               xmu_formula= x1~y+x2,
-#               xsigma_formula = ~1,
-#               xnu_formula = ~1,
-#               xtau_formula = ~1,
-#               xfamily = NO(mu.link = 'identity'),
-#               indicator = "indicator",
-#               censtype )
-
 # (Visualize draws) ------------------------------------------------------------------------------
 require(ggplot2)
 require(reshape2)
@@ -26,16 +10,17 @@ plotimputations <- function(df, boxes = TRUE) {
   df <- melt(df ,  id.vars = 'observation', variable.name = 'proposalVec')
 
   if (boxes) {
-    return(ggplot(df, aes(observation, value)) +
+    return(ggplot(data = subset(df, df$proposalVec != 'imputedx'), aes(observation, value)) +
              geom_boxplot(aes(group = observation)) +
-             ylab('Proposals for observation [i]'))
+             geom_point(data = subset(df, df$proposalVec == 'imputedx'), aes(observation, value, color = 'red'))+
+             ylab('Proposals for observation [i]')) # NOTE that red dots are based on mean, boxes display median.
   }else {
-    return(ggplot(df, aes(observation, value)) +
-             geom_point() +
+    return(ggplot() +
+             geom_point(data = subset(df, df$proposalVec != 'imputedx'), aes(observation, value)) +
+             geom_point(data = subset(df, df$proposalVec == 'imputedx'), aes(observation, value, color = proposalVec))+
              ylab('Proposals for observation [i]'))
   }
 }
 
-plotimputations(r$imputex$imputations[, -ncol(r$imputex$imputations)],
+plotimputations(d2$imputations[, -ncol(r$imputex$imputations)],
                 boxes = FALSE)  # the imputed value is ommited
-
