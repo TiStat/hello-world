f9a2ef80eed0da30fce3837f0349b40a6526ccff
Tim Ruhkopf <tim.ruhkopf@outlook.de>
2018-08-23 09:17:32 +0200

    Simulate data completely remastered.
    stark modularisiert und insbesondere damage argument interpretation komplett erneuert.
    unterscheidung lediglich via NA/list/...
    die unterscheidung ob uniformly random factors für jede beobachtung / oder fixed factors für alle obs. ist in eigene funktion ausgelagert.
    
    außerdem wird censtype automatisch ermittelt

--- R/simulatedata.R
+++ R/simulatedata.R
@@ -40,12 +40,18 @@
 # changes the probability to be defected. does not alter the independence assumption.
 #'@export 
 
-
 simulateData = function(n,
                         param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
                         defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =1/3),
                         family = 'NO',
                         correlation = NULL) {
+  
+  # extract all variable names from user specified formula
+  varnames = unique(unlist(sapply(param.formula, FUN = all.vars)))
+  
+  if(!(defect$name %in% varnames)){
+    stop('defected variable is not used in param.formula.')
+  }
   
   if(!identical(setdiff(names(defect), c('name','subset', 'prob', 'damage')), character(0))){
     stop('defect is not correctly specified')
@@ -59,26 +65,108 @@ simulateData = function(n,
     stop('at least one param.formula member is not a formula')
   }
   
-  if (is.list(defect$damage) && !all(sapply(defect$damage, FUN = is.numeric))){
-    stop('elements of defect$damage must be of same type')
-  }
-  # extract all variable names from user specified formula
-  varnames = sapply(param.formula, FUN = all.vars)
+  blankdata = generateblankdata(varnames, n, correlation)
   
-  if(!(defect$name %in% unlist(varnames))){
-    stop('defected variable is not used in param.formula.')
+  # evaluate the formulas on data.frame
+  param.frame = lapply(param.formula, FUN = function(x) eval(x[[2]], envir = blankdata))
+  param.frame$n = n
+  
+  if(!is.null(param.frame$sigma) && param.frame$sigma<0){
+    stop('sigma formula does not ensure positive sigma on all possible covariate values, which are from interval [0,1]')
+  } 
+  
+  # draw TRUE data from conditional family 
+  rfam = paste('r', family, sep= '')
+  if (any(!names(param.frame) %in% names(formals(rfam)))) {
+    stop('the specified parameter formulas do not match the required family\'s parameters')
+  }
+  y = do.call(rfam, param.frame)
+  
+  
+  defectdata = interpretdamage(blankdata, rule = defect)
+  indicator = defectdata$indicator
+  return(list(truedata = data.frame(y,blankdata,indicator), 
+              defected = cbind(y, defectdata$defected, indicator),
+              censtype = defectdata$censtype))
+}
+
+
+#' @description Takes the defect argument and interprets it on the
+#' @param rule defect argument of simulate data
+#' @return list. Defected dataframe and censtype.
+interpretdamage = function(truedata, rule){
+  
+  # @description function differentiates, if input is a skalar or vektor
+  # @return if x is skalar, return x. if x is a vector of length two,
+  # drawn n dimensional vector from unifom on interval [x[1], x[2]]
+  f = function(x, n){ # differentiate random /fix
+    if (length(x) == 1) { # fix
+      return(multiply = x)
+    } else if (length(x) == 2 ){ # random
+      return(multiply = runif(n, x[1], x[2]))
+    } else{
+      stop('defect$damage length is incorrect')
+    }
   }
   
-  if(!is.null(correlation) && 
-     nrow(correlation)!= unique(unlist(varnames))&& 
-     nrow(correlation)!= ncol(correlation)){
-    stop('The specified correlation matrix is either not square, its nrow is larger than the number of specified variables in param.formula')
+  # subset of observations to be defected with prob.
+  indicator = rep(0,nrow(truedata))
+  indicator[eval(rule$subset[[2]], envir = truedata)] = 
+    rbinom(n = sum(eval(rule$subset[[2]], envir = truedata)), # sum(BOOLEAN)
+           size = 1, 
+           prob = rule$prob) 
+  
+  if (all(is.na(rule$damage))) {
+    censtype = 'missing'
+    truedata[rule$name][indicator == 1,] = NA
+    return (list( defected = truedata, 
+                  censtype = censtype,
+                  indicator = indicator))
   }
   
-  # draw correlated data 
-  # according to: https://www.r-bloggers.com/easily-generate-correlated-variables-from-any-distribution-without-copulas/
+  n = sum(indicator) # sum(boolean)
+  subsetdefect =  truedata[rule$name][indicator == 1,]
+  
+  if (is.list(rule$damage)) {
+    censtype = 'interval'
+    truedata$lower = 0
+    truedata$lower[indicator == 1] = f(rule$damage[[1]], n) * subsetdefect
+    truedata[rule$name][indicator == 1,] = f(rule$damage[[2]], n) * subsetdefect
+    
+  } else {
+    multiply = f(rule$damage, n)
+    censtype = ifelse(all(multiply < 1), 'right', 'left')
+    truedata[rule$name][indicator == 1,] = multiply * subsetdefect
+  }
+  return (list( defected = truedata, 
+                censtype = censtype,
+                indicator = indicator))
+}
+
+#' @description This function generates a blank data.frame with uniformly
+#'   distributed variables specified in varnames. The variables are drawn i.i.d.
+#'   uniform distrib. if correlation = NULL, and are drawn from a correlated
+#'   uniform distrib. if a correlation matrix is supplied. The algorithm for the
+#'   introduction of correlation is follows to:
+#'   https://www.r-bloggers.com/easily-generate-correlated-variables-from-any-distribution-without-copulas/
+#'
+#' @param n integer. Number of observations
+#' @param varnames character vector. specifies variables to be created
+#' @param correlation symmetric correlation matix
+#' @return data.frame
+generateblankdata <- function(varnames, n, correlation= NULL) {
   if(!is.null(correlation)){
-    mu = rep(0,length(unique(unlist(varnames))))
+    
+    # check valid correlation matrix
+    if(!is.matrix(correlation)){
+      stop('correlation is not a matrix')
+    } else if(nrow(correlation)!= ncol(correlation)){
+      stop('correlation matrix is not square')
+    } else if (all(correlation != t(correlation))){
+      stop('correlation matrix is not symmetric')
+    }
+    
+    mu = rep(0,length(varnames))
     rvars = mvrnorm(n = n, mu = mu, Sigma = correlation)
     pvars = pnorm(rvars)
     rawdata = data.frame(qunif(pvars))
@@ -88,122 +176,57 @@ simulateData = function(n,
     rawdata = data.frame(matrix(runif(n*length(varnames)), 
                                 ncol = length(varnames)))
   }
-  names(rawdata) = unique(unlist(varnames))
+  names(rawdata) = varnames
   
-  # evaluate the formulas on data.frame
-  param.frame = list()
-  for (i in 1:length(param.formula)){
-    param.frame[[i]] = eval(param.formula[[i]][[2]], envir = rawdata)
-  }
-  names(param.frame) = names(param.formula)
-  
-  if(!is.null(param.frame$sigma) && param.frame$sigma<0){
-    stop('sigma formula does not ensure positive sigma on all possible covariate values, which are uniformly distributed on the interval [0,1]')
-  } 
-  
-  param.frame$n = n
-  
-  # draw TRUE data from conditional family 
-  rfam = paste('r', family, sep= '')
-  if (any(!names(param.frame) %in% names(formals(rfam)))) {
-    stop('the specified parameter formulas do not match the required family\'s parameters')
-  }
-  y = do.call(rfam, param.frame)
-  
-  # subset of observations to be defected with prob.
-  indicator = rep(0,nrow(rawdata))
-  indicator[eval(defect$subset[[2]], envir = rawdata)] = 
-    rbinom(n = sum(eval(defect$subset[[2]], envir = rawdata)),
-           size = 1, 
-           prob = defect$prob) # note sum(BOOLEAN)
-  
-  newdata = rawdata
-  if(any(is.na(defect$damage))){ # MISSING
-    newdata[defect$name][indicator== 1,] = NA
-    
-  } else if (!is.list(defect$damage) && length(defect$damage)==1){ # LEFT/RIGHT fixed factor
-    newdata[defect$name][indicator== 1,] = 
-      newdata[defect$name][indicator == 1,]*defect$damage
-    
-  } else if (!is.list(defect$damage) && length(defect$damage) == 2){ # LEFT/RIGHT random factors
-    newdata[defect$name][indicator == 1,] = 
-      newdata[defect$name][indicator == 1,] * 
-      runif(n = sum(indicator), min = defect$damage[1], max = defect$damage[2])
-    
-  } else if (is.list(defect$damage)){ # INTERVAL
-    newdata$lower = rep(0, n)
-    if (all(unlist(lapply(defect$damage, length))==1)){ # fixed factor
-      # lower : as START time
-      newdata$lower[indicator == 1] = 
-        newdata[defect$name][indicator == 1,]*defect$damage[[1]]
-      
-      # upper : defect$name as END time
-      newdata[defect$name][indicator== 1,] = 
-        newdata[defect$name][indicator == 1,]*defect$damage[[2]]
-      
-    }else if (all(unlist(lapply(defect$damage, length))==2)){ # random factor
-      # lower
-      newdata$lower[indicator == 1] = 
-        newdata[defect$name][indicator == 1,] * 
-        runif(n = sum(indicator), min = defect$damage[[1]][1], max = defect$damage[[1]][2])
-      
-      # upper
-      newdata[defect$name][indicator == 1,] = 
-        newdata[defect$name][indicator == 1,] * 
-        runif(n = sum(indicator), min = defect$damage[[2]][1], max = defect$damage[[2]][2])
-    }
-  }
-  return(list(truedata = data.frame(y,rawdata, indicator), 
-              defected = data.frame(y,newdata, indicator)))
+  return(rawdata)
 }
 
-
 # (working calls)----------------------------------
-
+# 
 rinterval = simulateData(n= 300,
-                         param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
+                         param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)),
                          defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =list(c(0.3, 0.99), c(1.2,1.5))),
                          family = 'NO',
                          correlation = NULL)
+# 
+# rright = simulateData(n= 300,
+#                       param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)),
+#                       defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =c(0.3, 0.9)),
+#                       family = 'NO',
+#                       correlation = NULL)
+# 
+# rleft = simulateData(n= 300,
+#                      param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)),
+#                      defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =c(1.2, 1.5)),
+#                      family = 'NO',
+#                      correlation = NULL)
+# 
+# finterval = simulateData(n= 300,
+#                          param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)),
+#                          defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =list(1/3,4/3)),
+#                          family = 'NO',
+#                          correlation = NULL)
 
-rright = simulateData(n= 300,
-                      param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
-                      defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =c(0.3, 0.9)),
-                      family = 'NO',
-                      correlation = NULL)
+# fright = simulateData(n= 300,
+#                       param.formula = list(mu = ~exp(x1)+ x2+ x3, sigma = ~sin(x2)),
+#                       defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =1/3),
+#                       family = 'NO',
+#                       correlation = matrix(c(1,0.3,0.2,
+#                                              0.3,1, 0.4,
+#                                              0.2,0.4,1), nrow = 3))
 
-rleft = simulateData(n= 300,
-                     param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
-                     defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =c(1.2, 1.5)),
-                     family = 'NO',
-                     correlation = NULL)
+# fleft = simulateData(n= 300,
+#                      param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)),
+#                      defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =4/3),
+#                      family = 'NO',
+#                      correlation = NULL)
 
-finterval = simulateData(n= 300,
-                         param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
-                         defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =list(1/3,4/3)),
-                         family = 'NO',
-                         correlation = NULL)
-
-fright = simulateData(n= 300,
-                      param.formula = list(mu = ~exp(x1)+ x2+ x3, sigma = ~sin(x2)), 
-                      defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =1/3),
-                      family = 'NO',
-                      correlation = matrix(c(1,0.3,0.2,
-                                             0.3,1, 0.4,
-                                             0.2,0.4,1), nrow = 3))
-
-fleft = simulateData(n= 300,
-                     param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
-                     defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =4/3),
-                     family = 'NO',
-                     correlation = NULL)
-
-missing = simulateData(n= 300,
-                       param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
-                       defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =NA),
-                       family = 'NO',
-                       correlation = NULL)
-any(is.na(missing$defected$x1))
+# missing = simulateData(n= 300,
+#                        param.formula = list(mu = ~exp(x1), sigma = ~sin(x2)), 
+#                        defect = list(name = 'x1', subset = ~ x1 > 0.6, prob = 0.8 , damage =NA),
+#                        family = 'NO',
+#                        correlation = NULL)
+# any(is.na(missing$defected$x1))
 
 
 
